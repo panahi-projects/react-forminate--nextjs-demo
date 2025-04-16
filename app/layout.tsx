ada0e1a26aea719a7f79d015fbd16036b0cbbacc
SAEED.PANAHI <panahi.projects@gmail.com>
2025-04-16 14:05:39 +0330

    add main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+import "#/styles/globals.css";
+import { AddressBar } from "#/ui/address-bar";
+import Byline from "#/ui/byline";
+import { GlobalNav } from "#/ui/global-nav";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +26,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" className="[color-scheme:dark]">
+      <body className="bg-gray-1100 overflow-y-scroll bg-[url('/grid.svg')] pb-36">
+        <GlobalNav />
+
+        <div className="lg:pl-72">
+          <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:px-8 lg:py-8">
+            <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
+              <div className="rounded-lg bg-black">
+                <AddressBar />
+              </div>
+            </div>
+
+            <div className="bg-vc-border-gradient rounded-lg p-px shadow-lg shadow-black/20">
+              <div className="rounded-lg bg-black p-3.5 lg:p-6">{children}</div>
+            </div>
+            <Byline />
+          </div>
+        </div>
       </body>
     </html>
   );
