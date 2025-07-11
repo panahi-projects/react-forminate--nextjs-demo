f9baeb61914c1e26bcc1822d3650ade76c4ae9b3
SAEED.PANAHI <panahi.projects@gmail.com>
2025-07-11 18:52:57 +0330

    add codesandbox iFrame to the page for the test

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,19 @@ export default function Home() {
     <div className="text-vercel-orange">
       {/* <Example1 /> */}
       Home
+      <iframe
+        src="https://codesandbox.io/sS/github/panahi-projects/react-forminate--nextjs-demo/main?autoresize=1&fontsize=14&hidenavigation=1&module=/app/page.tsx&runonclick=1"
+        style={{
+          width: "100%",
+          height: "500px",
+          border: "0",
+          borderRadius: "4px",
+          overflow: "hidden",
+        }}
+        title="panahi-projects/react-forminate--nextjs-demo/main"
+        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+      ></iframe>
     </div>
   );
 }
